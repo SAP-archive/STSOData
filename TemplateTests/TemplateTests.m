5fc551f8e652368a9915b37ba977fdce0cc78a71
Stan Stadelman <i826181@sap.com>
2014-08-06 10:23:28 -0700

    added Tests folder
    Change-Id: Icccb3677e2f50be06491addec3878a2621847dd1

---
+++ TemplateTests/TemplateTests.m
@@ -0,0 +1,34 @@
+//
+//  TemplateTests.m
+//  TemplateTests
+//
+//  Created by Stadelman, Stan on 8/4/14.
+//  Copyright (c) 2014 Stan Stadelman. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TemplateTests : XCTestCase
+
+@end
+
+@implementation TemplateTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
